400aeca2bc9cff714d674b2a204e1a4e6fc9247f
Benginio <jeanbaptistebenginio@gmail.com>
2023-03-11 03:35:06 -0500

    commit contraint for the field if empty

--- app/src/main/java/com/benginio/flashcard1/AddCardActivity.java
+++ app/src/main/java/com/benginio/flashcard1/AddCardActivity.java
@@ -4,11 +4,13 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.text.TextUtils;
 import android.view.View;
 import android.widget.EditText;
+import android.widget.TextView;
+import android.widget.Toast;
 
 public class AddCardActivity extends AppCompatActivity {
-
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -24,15 +26,20 @@ public class AddCardActivity extends AppCompatActivity {
         findViewById(R.id.flashcard_save_button).setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                Intent data=new Intent();
-              String inputQuestion  =((EditText)  findViewById(R.id.flashcard_question_edittext)).getText().toString();
-                String inputAnswer  =((EditText)  findViewById(R.id.flashcard_answer_edittext)).getText().toString();
-                data.putExtra("QUESTION_KEY", inputQuestion );
-                data.putExtra("ANSWER_KEY", inputAnswer );
+                if(TextUtils.isEmpty(((EditText)  findViewById(R.id.flashcard_question_edittext)).getText().toString())){
+                    Toast.makeText(AddCardActivity.this, "Question field is null!", Toast.LENGTH_SHORT).show();
+                }else if(TextUtils.isEmpty(((EditText)  findViewById(R.id.flashcard_answer_edittext)).getText().toString())){
+                    Toast.makeText(AddCardActivity.this, "Answer field is null!", Toast.LENGTH_SHORT).show();
+                }else {
+                    Intent data = new Intent();
+                    String inputQuestion = ((EditText) findViewById(R.id.flashcard_question_edittext)).getText().toString();
+                    String inputAnswer = ((EditText) findViewById(R.id.flashcard_answer_edittext)).getText().toString();
+                    data.putExtra("QUESTION_KEY", inputQuestion);
+                    data.putExtra("ANSWER_KEY", inputAnswer);
 
-                setResult(RESULT_OK,data);
-                finish();
-
+                    setResult(RESULT_OK, data);
+                    finish();
+                }
             }
         });
 
